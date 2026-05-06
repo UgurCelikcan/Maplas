68e1ecd091a1467f31cdedfb34e6a0eda5534707
UgurCelikcan <ugurandac730@gmail.com>
2026-05-06 14:30:21 +0300

    hotfix

--- src/leaflet-setup.ts
+++ src/leaflet-setup.ts
@@ -4,6 +4,14 @@ import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 
+// Fix for default marker icons in Vite/Webpack
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
+
 // @ts-ignore
 window.L = L;
 
